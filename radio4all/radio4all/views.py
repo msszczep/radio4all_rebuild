0b74507e2a7d7f97d9bbb581a669cae0a389bd7d
Mitchell Szczepanczyk <mitchell@szcz.org>
2021-11-04 19:27:46 -0500

    fix hidden entries on advisory pages

--- radio4all/radio4all/views.py
+++ radio4all/radio4all/views.py
@@ -1579,7 +1579,7 @@ def filter_advisory(request, advisory_id):
     advisory_strings = {1: 'Unknown - program has not been checked for content', 2: 'No Advisories - content screened and verified', 3: 'Warning: Program may contain strong or potentially offensive language, including possible FCC violations.', 4: 'Warning: Program content only suitable for FCC-designated safe harbor (10PM to 6AM).'}
     advisory_to_use = advisory_strings[advisory_id]
     try:
-        target = Programs.objects.filter(advisory=advisory_id).order_by('-date_created')
+        target = Programs.objects.filter(advisory=advisory_id, hidden=0).order_by('-date_created')
         paginator = Paginator(target, 30)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
