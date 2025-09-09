03fdeb75727be3565853eb200d62af30d0fb4a78
Mitchell Szczepanczyk <mitchell@szcz.org>
2025-09-08 20:47:40 -0500

    Fixing embarassing bugs

--- radio4all/radio4all/views.py
+++ radio4all/radio4all/views.py
@@ -2081,7 +2081,7 @@ def mobile_browse(request):
 def mobile_program(request, pk):
     now = timezone.now()
     p = Programs.objects.get(program_id=pk)
-    if p.date_published < now
+    if p.date_published > now:
         return HttpResponse('<h1>No Programs Here</h1>')
     versions = Versions.objects.filter(program_id=pk)
     ftu = Files.objects.filter(program_id=pk)
